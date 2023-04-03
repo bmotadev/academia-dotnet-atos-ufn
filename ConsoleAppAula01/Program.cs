6b2aa7a5d772257c2808463855abeea9d31adb00
Bruno Mota <bmotadev@gmail.com>
2023-04-03 15:48:40 -0300

    Add project files.

---
+++ ConsoleAppAula01/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleAppAula01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escreva o seu nome: ");
+
+            string nome;
+            nome = "Bruno";
+
+            Console.WriteLine(nome);
+        }
+    }
+}
