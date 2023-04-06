6f1ca067d6ac35c2ccb63030701531e85aa5e6c3
Bruno Mota <bmotadev@gmail.com>
2023-04-06 14:22:58 -0300

    :sparkles: feat: Aula 04 - Switch-Case - Iniciada

---
+++ ConsoleAppAula04/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ConsoleAppAula04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Switch-case
+
+            int num;
+            Console.WriteLine("Digite um numero:");
+            num = int.Parse(Console.ReadLine());
+
+            switch(num)
+            {
+                case 9:
+                    Console.WriteLine("O numero digitado é 9");
+                    break;
+                case 10:
+                    Console.WriteLine("O numero digitado é 10");
+                    break;
+                default:
+                    Console.WriteLine("O numero digitado não é 9 nem 10");
+                    break;
+            }
+        }
+    }
+}
