c9d51552b17ae756e314c73b2c08497febe307f4
Bruno Mota <bmotadev@gmail.com>
2023-04-11 14:40:25 -0300

    :tada: Aula 06, iniciada

--- ConsoleAppAula05/Program.cs
+++ ConsoleAppAula05/Program.cs
@@ -8,8 +8,45 @@
 
             // escreva um algoritmo que mostre todos os números de 0 a 10.
 
-            for (int i = 0; i <= 10; i++)
+            //for (int i = 0; i <= 10; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+
+            /*
+             * Escreva um algoritmo que mostre todos os números de 20 a 5.
+             * 
+             * **/
+
+            //for(int i = 20; i >= 5; i--)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+
+            /*
+             * Escreva um algoritmo leia pelo teclado os dois valores inteiros chamados inicio e fim, e então mostre todos os números entre inicio e fim.
+             * **/
+
+            //int inicio = int.Parse(Console.ReadLine());
+            //int fim = int.Parse(Console.ReadLine());
+
+            //for(int i = inicio; i <= fim; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+
+            /*
+             * Escreva um algoritmo leia pelo teclado um valor inteiro chamado x, e então mostre na tela todos os números de 0 a 100, pulando de x em x.
+             * **/
+
+            int valorX = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i <= 100; i+= valorX)
             {
+                
                 Console.WriteLine(i);
             }
         }
---
+++ ConsoleAppAula06/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleAppAula06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, op;
+
+            for(i = 0; i < 10; i++)
+            {
+                if(i == 3)
+                {
+                    continue;
+                }
+                if (i == 7)
+                {
+                    break;
+                }
+
+                Console.WriteLine("i vale" + i);
+            }
+        }
+    }
+}
