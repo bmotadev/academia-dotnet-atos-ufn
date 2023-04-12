81c7176883f79c05c5e06c4d67680b2a54c9de49
Bruno Mota <bmotadev@gmail.com>
2023-04-12 15:29:14 -0300

    :sparkles: feat: Aula 07 - Iniciada

--- ConsoleAppAula06/Program.cs
+++ ConsoleAppAula06/Program.cs
@@ -4,21 +4,35 @@
     {
         static void Main(string[] args)
         {
-            int i, op;
+            //int i, op;
 
-            for(i = 0; i < 10; i++)
+            //for(i = 0; i < 10; i++)
+            //{
+            //    if(i == 3)
+            //    {
+            //        continue;
+            //    }
+            //    if (i == 7)
+            //    {
+            //        break;
+            //    }
+
+            //    Console.WriteLine("i vale" + i);
+            //}
+
+
+            /*
+             *  Escreva um algoritmo que leia pelo teclado o valor de uma variavel inteira chamada opcao.
+             *  Porem o programa só pode seguir sua execução caso o valor de opcao seja entre 1 e 4
+             *  Se o valor digitado não for entre 1 e 4, a variável opcao devera ser lida novamente pelo teclado.
+             * **/
+
+            int opcao;
+            do
             {
-                if(i == 3)
-                {
-                    continue;
-                }
-                if (i == 7)
-                {
-                    break;
-                }
-
-                Console.WriteLine("i vale" + i);
-            }
+                Console.WriteLine("Informe uma opção entre 1 e 4");
+                opcao = int.Parse(Console.ReadLine());
+            } while (opcao < 1 || opcao > 4);
         }
     }
 }
---
+++ ConsoleAppAula07/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace ConsoleAppAula07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                int x = 0;
+
+
+
+                while (x <= 0)
+                {
+                    Console.WriteLine("Informe um valor positivo:");
+                    x = int.Parse(Console.ReadLine());
+                }
+
+
+
+                int soma = 0;
+                int contagem = 0;
+
+
+
+                for (int i = 1; i < x; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        Console.Write(i + ", ");
+                        contagem++;
+                        soma += i; // soma = soma + i;
+                    }
+                }
+
+
+
+                Console.WriteLine("Deseja informar outr número s/n");
+                string resposta = Console.ReadLine();
+
+
+
+                if (resposta == "n")
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
