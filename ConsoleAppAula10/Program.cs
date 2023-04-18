c4bee60de9ef87d55d7035b83ed07fea5e6563aa
Bruno Mota <bmotadev@gmail.com>
2023-04-18 14:49:05 -0300

    :sparkles: feat: Aula 10 Funcoes - Iniciada

---
+++ ConsoleAppAula10/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace ConsoleAppAula10
+{
+    using System.Runtime.Serialization;
+    namespace Funcoes
+    {
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                double n1, n2, res;
+                Console.WriteLine("Digite dois numeros: ");
+                n1 = double.Parse(Console.ReadLine());
+                n2 = double.Parse(Console.ReadLine());
+                soma(n1, n2);
+                res = multiplica(n1, n2);
+                Console.WriteLine(n1 + "*" + n2 + "=" + res);
+                subtrai();
+                divide();
+            }
+            public static void subtrai()
+            {
+                Console.WriteLine("Aqui eu estou na função subtrai");
+            }
+            public static void soma(double num1, double num2)
+            {
+                double resultado;
+                resultado = num1 + num2;
+                Console.WriteLine(num1 + "+" + num2 + "=" + resultado);
+            }
+            public static void divide()
+            {
+                Console.WriteLine("Aqui eu estou na função divide");
+            }
+            public static double multiplica(double num1, double num2)
+            {
+                double resultado;
+                resultado = num1 * num2;
+                return resultado;
+            }
+            
+        }
+    }
+}
