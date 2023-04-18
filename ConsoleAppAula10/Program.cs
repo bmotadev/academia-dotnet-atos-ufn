82592232e8066153d5d13734ce33bebe3f008929
Bruno Mota <bmotadev@gmail.com>
2023-04-18 15:35:03 -0300

    :sparkles: feat: aula 10 funcoes - verificando par

--- ConsoleAppAula10/Program.cs
+++ ConsoleAppAula10/Program.cs
@@ -5,21 +5,11 @@
     {
         internal class Program
         {
-            static void Main(string[] args)
+            public static double subtrai(double num1, double num2)
             {
-                double n1, n2, res;
-                Console.WriteLine("Digite dois numeros: ");
-                n1 = double.Parse(Console.ReadLine());
-                n2 = double.Parse(Console.ReadLine());
-                soma(n1, n2);
-                res = multiplica(n1, n2);
-                Console.WriteLine(n1 + "*" + n2 + "=" + res);
-                subtrai();
-                divide();
-            }
-            public static void subtrai()
-            {
-                Console.WriteLine("Aqui eu estou na função subtrai");
+                double resultado;
+                resultado = num1 - num2;
+                return resultado;
             }
             public static void soma(double num1, double num2)
             {
@@ -27,9 +17,11 @@
                 resultado = num1 + num2;
                 Console.WriteLine(num1 + "+" + num2 + "=" + resultado);
             }
-            public static void divide()
+            public static double divide(double num1, double num2)
             {
-                Console.WriteLine("Aqui eu estou na função divide");
+                double resultado;
+                resultado = num1 / num2;
+                return resultado;
             }
             public static double multiplica(double num1, double num2)
             {
@@ -37,7 +29,32 @@
                 resultado = num1 * num2;
                 return resultado;
             }
-            
+            public static bool verificaPar(double numero)
+            {
+                if (numero % 2 == 0)
+                    return true;
+                else return false;
+            }
+            static void Main(string[] args)
+            {
+                double n1, n2, res;
+                Console.WriteLine("Digite dois numeros: ");
+                n1 = double.Parse(Console.ReadLine());
+                n2 = double.Parse(Console.ReadLine());
+                soma(n1, n2);
+                res = subtrai(n1, n2);
+                Console.WriteLine(n1 + "-" + n2 + "=" + res);
+                res = multiplica(n1, n2);
+                Console.WriteLine(n1 + "*" + n2 + "=" + res);
+                Console.WriteLine(n1 + "/" + n2 + "=" + divide(n1, n2));
+                if (verificaPar(n1))
+                    Console.WriteLine("Par");
+                else Console.WriteLine("Impar");
+                if (verificaPar(n2))
+                    Console.WriteLine("Par");
+                else Console.WriteLine("Impar");
+            }
+
         }
     }
 }
