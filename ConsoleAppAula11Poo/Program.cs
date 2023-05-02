0d3163ad100dab4d9c021b77acc0cc654a8b0a8c
Bruno Mota <bmotadev@gmail.com>
2023-05-02 14:44:46 -0300

    Primeira aula de POO iniciada

---
+++ ConsoleAppAula11Poo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleAppAula11Poo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá, estou no meu primeiro programa OO");
+        }
+    }
+}
