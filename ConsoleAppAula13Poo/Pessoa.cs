0d03ad78061a5ff54c9447fed2306ed7f6022c4f
Bruno Mota <bmotadev@gmail.com>
2023-05-08 14:38:21 -0300

    :sparkles: feat: criado a classe pessoa

---
+++ ConsoleAppAula13Poo/Pessoa.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppAula13Poo
+{
+    internal class Pessoa
+    {
+        /*Encapsulamento é um principio da orientação a objetos         * Permite proteger informações de um objeto        public -> vai ser publico, vai ser visivel a todos os componentes do propgrama        private -> é acessivel somente dentro da classe        protected -> é acessivel dentro da prórpia classe e nas suas subclasses (herança)        internal -> podem ser acessado dentro do mesmo assembly (um conjunto de aquivos de código fontes        que são compilados juntos)1 - Criar uma classe chamada Pessoa com 2 construtores, um que receba o nome e a idade da pessoa e outro recebendo apenas a idade. Solicite ao usuário qual dos construtores ele gostaria de utilizar na instanciação da classe. Além de receber parâmetros, os construtores imprimem na tela o conteúdo dos parâmetros recebidos         */
+        public string nome;
+        private int idade;
+        public Pessoa(string nome, int idade)
+        {
+            this.nome = nome;
+            this.idade = idade;
+            Console.WriteLine("Nome: " + nome + " Idade: " + idade);
+        }
+        public Pessoa(int idade)
+        {
+            this.idade = idade;
+            Console.WriteLine("Idade: " + idade);
+        }
+    }
+}
