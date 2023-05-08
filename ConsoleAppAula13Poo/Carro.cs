4397a6b290932d64dd48fee823ca7d3a58bd3213
Bruno Mota <bmotadev@gmail.com>
2023-05-08 15:14:54 -0300

    :sparkles: classe carro completa

--- ConsoleAppAula13Poo/Carro.cs
+++ ConsoleAppAula13Poo/Carro.cs
@@ -11,6 +11,42 @@ namespace ConsoleAppAula13Poo
         private string marca;
         private string modelo;
         private int ano;
+        private Motor m;
+
+        public string Marca 
+        { 
+            get 
+            { 
+                return marca; 
+            } 
+            set 
+            {  
+                marca = value; 
+            } 
+        }
         
+        public string Modelo
+        {
+            get
+            {
+                return modelo;
+            }
+            set
+            {
+                modelo = value;
+            }
+        }
+
+        public int Ano
+        {
+            get
+            {
+                return ano;
+            }
+            set
+            {
+                ano = value;
+            }
+        }
     }
 }
