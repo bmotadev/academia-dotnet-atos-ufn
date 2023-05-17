ed7826c9129f41a72981d1c16e1a691a2fa1fb27
Bruno Mota <bmotadev@gmail.com>
2023-05-17 12:39:23 -0300

    :sparkles: feat: classe motocicleta criada

---
+++ ConsoleAppAula14Poo/Motocicleta.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppAula14Poo
+{
+    internal class Motocicleta : Veiculo
+    {
+        public override void Ligar()
+        {
+            Console.WriteLine("Moto ligada");
+        }
+    }
+}
