a73a17919e867bef598629d8f9b2486d9a9dd338
Bruno Mota <bmotadev@gmail.com>
2023-05-17 12:55:54 -0300

    :sparkles: feat: Criado a classe pessoa

---
+++ ConsoleAppAula14Poo/Pessoa.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppAula14Poo
+{
+    internal class Pessoa
+    {
+        public string Nome;
+        public Endereco end;
+        public List<Cachorro> caes;
+        public Pesssoa()
+        {
+            Nome = Nome;
+        }
+    }
+}
